13128752dde9e5df7f97519d9d8fa69d71944dab
ErastoApolnary <galaboy17@gmail.com>
2024-05-24 02:27:58 -0700

    Add files via upload

---
+++ create.php
@@ -0,0 +1,18 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $phone = $_POST['phone'];
+
+    $sql = "INSERT INTO customers (name, email, password, phone) VALUES ('$name', '$email', '$password', '$phone')";
+
+    if ($conn->query($sql) === TRUE) {
+        header('Location: index.php');
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+?>
